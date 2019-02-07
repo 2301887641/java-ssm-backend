1644e9f6fbe52785867e261d79cfedc9ff2c56bb
sui <2301887641@qq.com>
2019-02-07 13:26:41 +0800

    添加shiro

--- mall-portal/src/main/java/com/mall/controller/portal/LoginController.java
+++ mall-portal/src/main/java/com/mall/controller/portal/LoginController.java
@@ -3,10 +3,12 @@ package com.mall.controller.portal;
 import com.mall.core.annotation.DoValid;
 import com.mall.core.constant.ConstantsPool;
 import com.mall.core.foundation.Result;
+import com.mall.core.util.FrontUtil;
 import com.mall.group.userDto.ValidationUserDto;
 import com.mall.service.api.UserService;
 import com.mall.service.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,16 +20,26 @@ import javax.servlet.http.HttpSession;
 
 /**
  * 登录相关
+ *
  * @author suiguozhen on 19/01/19 15:06
  */
-@RestController
+@Controller
 public class LoginController {
+
+    private static final String TEMPLATE_DIR = "login";
+    private static final String TEMPLATE_NAME = "login";
+
     @Autowired
     private UserService userService;
 
+    @GetMapping("/login.html")
+    public String toLogin() {
+        return FrontUtil.getTemplatePath(TEMPLATE_DIR,TEMPLATE_NAME);
+    }
+
     @PostMapping("/login.do")
     @DoValid
-    public Result<Void> doLogin(HttpSession session, @Validated({ValidationUserDto.ValidationFrontUserLogin.class}) UserDto userDto, BindingResult result){
+    public Result<Void> doLogin(HttpSession session, @Validated({ValidationUserDto.ValidationFrontUserLogin.class}) UserDto userDto, BindingResult result) {
 //        UserDto user = userService.getByUsernameAndPassword(userDto.getUsername(),SecurityUtil.messageDigest(userDto.getPassword()));
 //        if(Objects.isNull(user)){
 //            return Result.failed(SpringUtil.getMessage("validation.user.isNull"));
@@ -37,14 +49,8 @@ public class LoginController {
     }
 
     @GetMapping("/logout.do")
-    public Result<Void> logout(HttpSession session){
+    public Result<Void> logout(HttpSession session) {
         session.removeAttribute(ConstantsPool.Session.USER_SESSION_NAME);
         return Result.success();
-    }
-
-    @GetMapping("/register/{subject}/{type}.do")
-    public Result<String> verifyUser(@PathVariable String subject,@PathVariable String type){
-
-        return Result.failed();
     }
 }
