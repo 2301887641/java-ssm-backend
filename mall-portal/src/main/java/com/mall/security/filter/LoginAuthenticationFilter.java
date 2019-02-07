711f6b29db10ab215995c9fdca24b6bd7e0ec3e7
sui <2301887641@qq.com>
2019-02-07 22:51:31 +0800

    添加shiro

--- mall-portal/src/main/java/com/mall/security/filter/LoginAuthenticationFilter.java
+++ mall-portal/src/main/java/com/mall/security/filter/LoginAuthenticationFilter.java
@@ -1,9 +1,12 @@
 package com.mall.security.filter;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * 登录拦截 Form表单的身份验证过滤器
@@ -13,5 +16,16 @@ public class LoginAuthenticationFilter extends FormAuthenticationFilter {
     @Override
     protected boolean executeLogin(ServletRequest request, ServletResponse response) throws Exception {
         return super.executeLogin(request, response);
+    }
+
+    @Override
+    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpSession session = httpServletRequest.getSession();
+        String captcha = httpServletRequest.getParameter("captcha");
+        if(StringUtils.isNotBlank(captcha)){
+
+        }
+        return super.onAccessDenied(request, response);
     }
 }
